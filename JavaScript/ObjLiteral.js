f8e6d4f1ebfe57b73a900c17ec6e7cf39f3bc66a
Nitesh <niteshsingh7343@gmail.comgit config --global user.name Nitesh>
2025-11-16 13:23:08 +0530

    Studiedn about abject literals

---
+++ JavaScript/ObjLiteral.js
@@ -0,0 +1,17 @@
+let students = {
+    Name: "Nitesh",
+    Age: 21,
+    Makrs: 98
+};
+
+let item ={
+    Name: "Mobile",
+    Price: 45000,
+    Color: ["Rose","Black","White","Biege"],
+    Brand: "Samsung",
+    Type: "Flexib",
+    Discouont: 15,
+    Warranty: "1 Year",
+    RAM: [8,12,16],
+    Storage: [128,256,512,1024]
+};
---
+++ JavaScript/Post.js
@@ -0,0 +1,28 @@
+const post = {
+    Username: "_i.am.panda_",
+    Content: "This is my first post.",
+    Likes: 150,
+    Reposts: 5,
+    tags: ["@nitesh","@ashu"]
+};
+
+
+const nitesh = {
+    insta: {
+        username: "_the.nitesh",
+        email: "niteshsingh7343@gmail.com",
+        password: "12345@12345",
+    },
+
+    mobile :{
+        brand: "Samsung",
+        model: "S23 FE",
+        color: "Space Grey",
+    },
+
+    laptop: {
+        brand : "Apple",
+        model : "Macbook Air M1",
+        color : "Silver Metal",
+    }
+}
