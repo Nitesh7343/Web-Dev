268c2bc181766922549f8afc87b46055045bcf6d
Nitesh <niteshsingh7343@gmail.comgit config --global user.name Nitesh>
2025-11-18 00:50:57 +0530

    JS Functons ongoing

--- JavaScript/diceRoll.js
+++ JavaScript/diceRoll.js
@@ -19,12 +19,37 @@
 // printInfo("NItesh");
 
 
-function Nsum(n) {
-    let sum = 0;
-    for(let i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
-}
+// function Nsum(n) {
+//     let sum = 0;
+//     for(let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
 
-console.log(Nsum(10));
+// console.log(Nsum(10));
+
+
+// let sum = function() {
+//     return 2+3;
+// }
+
+// console.log(sum());
+
+// let check = (age) => {
+//     if(age >= 18) {
+//         return "You can vote"
+//     }
+//     else 
+//         return `You can vote after ${18-age} Years`
+// };
+
+// console.log(check(13));
+
+console.log("Hello!");
+
+setInterval(() => {
+    console.log("Panda")
+},4000);
+
+console.log("I'm");
