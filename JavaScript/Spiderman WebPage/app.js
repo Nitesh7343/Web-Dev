bf8a64d7f57681dd7ad2636a0c7ac6c7decca37d
Nitesh <niteshsingh7343@gmail.com>
2025-11-19 23:52:56 +0530

    manipulation in Spider-Man webpage started using app.js

--- JavaScript/Spiderman WebPage/app.js
+++ JavaScript/Spiderman WebPage/app.js
@@ -1,6 +1,6 @@
-// let images = document.getElementsByClassName("oldImg");
-// for(let i = 0; i < images.length;i++) {
-//     images[i].src = "spider.png";
-//     console.log(`Image of ${i}th image is changes.`)
-// }
+let images = document.getElementsByClassName("oldImg");
+for(let i = 0; i < images.length;i++) {
+    images[i].src = "spider.png";
+    console.log(`Image of ${i}th image is changes.`)
+}
 
