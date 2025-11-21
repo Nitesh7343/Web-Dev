c05633458aece88bd2a30005443cde9a6fa89232
Nitesh <niteshsingh7343@gmail.com>
2025-11-21 19:08:00 +0530

    JS  DOM selectors completed

--- JavaScript/Spiderman WebPage/app.js
+++ JavaScript/Spiderman WebPage/app.js
@@ -4,5 +4,10 @@
 //     console.log(`Image of ${i}th image is changes.`)
 // }
 
-
+// let a = document.getElementsByClassName("boxLink");
+// console.log(typeof(a));
+// for(let i = 0; i < a.length;i++) {
+//     a[i].href = "https://www.google.com"
+//     console.log(a[i].href);
+// }
 
