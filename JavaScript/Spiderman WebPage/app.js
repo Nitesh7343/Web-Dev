f243e23c5f19993545f615eb5c2181f6f16600b0
Nitesh <niteshsingh7343@gmail.com>
2025-11-23 01:06:08 +0530

    operations like :- manipution , adding elements , removing elements - completed

--- JavaScript/Spiderman WebPage/app.js
+++ JavaScript/Spiderman WebPage/app.js
@@ -16,8 +16,16 @@
 //     a[i].style.color = "green";
 // }
 
-let a = document.querySelectorAll("a");
-console.log(a.parentElement);
+// let a = document.querySelectorAll("a");
+// console.log(a[0].parentElement);
+// let p = document.getElementsByTagName("p");
+// p[0].innerHTML = '\n        <b>Spider-Man</b> is a superhero appeared in a Amercial comic books published by <a href="#">Marvel Comics</a>.\n        Created by writer-editor <b>Stan Lee</b> ans artists <b>Steve Ditko</b>, hefirsrt appeared in the authology comic book <a href="#">Amazing Fantasy</a>\n        #15 (August 1962) in the <a href="#">Silver Age of comic book</a>. Hehas been featured in comic book,televison show,video games ,novels and plays.\n';
+
+// let box = document.querySelector("ul");
+// let add = document.createElement("li");
+// add.innerText = "Hello World";
+// box.appendChild(add);
+
 
 
 
