db13c3b3fd48c94da87cd4e54a60ba07755bf343
Nitesh <niteshsingh7343@gmail.com>
2025-12-02 23:47:52 +0530

    get request sent and received responses

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -3,8 +3,26 @@ const app = express();
 
 // console.dir(app);
 
-let port = 4000;
+let port = 8080;
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
+})
+
+app.get("/",(req,res) => {
+    console.log("Request Received on /");
+    res.send("Receiver on route /");
+});
+app.get("/apple",(req,res) => {
+    console.log("Request Received on apple");
+    res.send("Receiver on route apple");
+});
+app.get("/orange",(req,res) => {
+    console.log("Request Received on orange");
+    res.send("Receiver on route orange");
+});
+
+app.use((req,res) => {
+    console.log("Wrong path");
+    res.send("Wrong path");
 })
