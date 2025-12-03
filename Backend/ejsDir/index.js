d5aef81b7ed207d3a9e6b71d00b47d2cce3abef9
Nitesh <niteshsingh7343@gmail.com>
2025-12-03 11:42:22 +0530

    added path for access path dir form anywhere

--- Backend/ejsDir/index.js
+++ Backend/ejsDir/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
-
+const path = require("path");
 let app = express();
 
 let port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
 
 app.get("/",(req,res) => {
     console.log("Server working");
     res.render("home.ejs");
+});
+
+app.get("/hello",(req,res) => {
+    console.log("Server working");
+    res.send("Hemlo Baby");
 })
 
 app.listen(port, ()=> {
