01d670b5c496dee07eb5f2b7874c5bb24e3e0266
Nitesh <niteshsingh7343@gmail.com>
2025-12-07 17:06:41 +0530

    Making a Quara post page for which get api on /post route is completed and working.

---
+++ REST_CLASS/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+const path = require("path");
+
+app.use(express.urlencoded({ extended:true }));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username:"bunnny",
+        content:"bunny codes everything."
+    },
+    {
+        username:"dora",
+        content:"dora eats momos."
+    },
+    {
+        username:"panda",
+        content:"panda loves sleeping."
+    },
+    {
+        username:"kitty",
+        content:"kitty meows all day."
+    },
+    {
+        username:"kitkat",
+        content:"kitkat loves chocolates."
+    }
+]
+
+app.get("/posts",(req,res) => {
+    res.render("home.ejs",{posts});
+});
+
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
