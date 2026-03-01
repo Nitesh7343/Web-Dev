7311887831d76ad7733ebd58d40ba67c4d459116
Nitesh <niteshsingh7343@gmail.com>
2026-03-01 23:02:10 +0530

    UI Project using react fully

---
+++ REACT/react1/UIproject/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is Navbar
+    </div>
+  )
+}
+
+export default Navbar
---
+++ REACT/react1/UIproject/src/components/Section1/Section1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Section1 = () => {
+    return <div className='h-screen w-full'>
+        <Navbar/>
+    </div>
+}
+
+export default Section1
